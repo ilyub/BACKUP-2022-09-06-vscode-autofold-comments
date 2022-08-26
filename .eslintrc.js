a3a35bdcb61c54117b5344a08b51b60ba81a2c23
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-08-26 10:20:33 +0300

    chore(fix): Fix after "@skylib/config" update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
+  ignorePatterns: ["out/**"],
   extends: [
     "./node_modules/@skylib/config/eslint",
+    "./node_modules/@skylib/config/eslint/special-locations",
     "./.eslintrc.overrides",
     "./.eslintrc.rule-overrides",
     "./.eslintrc.temp"
